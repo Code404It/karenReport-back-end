c575efc16ef7dbe3d32f9659682ebfd3ec54b71c
code404it <jessikahd.smith@gmail.com>
2020-06-25 19:31:48 -0400

    updated karens params

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,17 +3,17 @@ class LocationsController < ApplicationController
 
     def index
         @locations = Location.all 
-        render json: @locations.as_json(include: {karens: {only: [:id,:title, :incident]}})
+        render json: @locations.as_json(include: {karens: {only: [:id, :title, :incident, :damn_karen, :location_id]}})
     end
 
     def show
-        render json: @location.as_json(include: {karens: {only: [:id,:title, :incident]}})
+        render json: @location.as_json(include: {karens: {only: [:id, :title, :incident, :damn_karen, :location_id]}})
     end
 
     def create
         @location = Location.new(location_params)
          if @location.save
-            render json: @location.as_json(include: {karens: {only: [:id,:title, :incident]}})
+            render json: @location.as_json(include: {karens: {only: [:id, :title, :incident, :damn_karen, :location_id]}})
          else
             render json: @location.errors 
          end
@@ -22,7 +22,7 @@ class LocationsController < ApplicationController
     def update
         @location = Location.new(location_params)
         if @location.update
-           render json: @location.as_json(include: {karens: {only: [:id,:title,:incident]}})
+           render json: @location.as_json(include: {karens: {only: [:id, :title, :incident, :damn_karen, :location_id]}})
         else
            render json: @location.errors 
         end
@@ -34,6 +34,6 @@ class LocationsController < ApplicationController
     end
 
     def location_params
-        params.require(:location).permit(:city, :state, :karens_attributes => [:id, :title, :incident, :location_id])
+        params.require(:location).permit(:city, :state, :created_at, :karens_attributes => [:id, :title, :incident, :damn_karen, :location_id])
     end
 end
